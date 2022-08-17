088e6922a2a61bb7d7bd598906542ba60c84dd04
Julio Cesar <caliaro3@homtail.com>
2022-08-16 22:54:21 -0300

    separando css e java script do html

---
+++ script.js
@@ -0,0 +1,32 @@
+const dog = new Audio('./audio/oi.mp3');
+        const away = new Audio('./audio/away.mp3');
+        const tiringa = new Audio('./audio/tiringa.mp3');
+    
+        
+        function playDog (nDeRepeticoes){
+            stopAll();
+            dog.play();
+            for (let i = 0; i < nDeRepeticoes; i = i+1) {
+                console.log("reproduzindo..." ,i);
+                
+            }
+            
+        
+        }
+        function playAway (){
+            //stopAll();
+            away.play();
+        }
+        function playTiringa (){
+            stopAll();
+            tiringa.play();
+        }
+
+        function stopAll (){
+            dog.pause();
+            dog.currentTime = 0;
+            away.pause();
+            away.currentTime = 0;
+            tiringa.pause();
+            tiringa.currentTime = 0;
+        }

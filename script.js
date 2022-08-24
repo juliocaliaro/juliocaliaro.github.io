f913dace41abbf960446f426b5baa4546c624e49
Julio Cesar <caliaro3@homtail.com>
2022-08-23 22:54:49 -0300

    adicionando novos memes e projetos, ajustou o menu para mobile e adicionou homepage

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
-const dog = new Audio('./audio/oi.mp3');
+        const dog = new Audio('./audio/oi.mp3');
         const away = new Audio('./audio/away.mp3');
         const tiringa = new Audio('./audio/tiringa.mp3');
-    
-        
-        function playDog (nDeRepeticoes){
+        const sefodeo = new Audio('./audio/sefodeo.mp3');
+        const calaboca = new Audio('./audio/calaboca.mp3');
+        const creditos= new Audio('./audio/creditos.mp3');
+
+
+        function playDog {
             stopAll();/*para*/
             dog.play();/*toca*/
-            for (let i = 0; i < nDeRepeticoes; i = i+1) {
-                console.log("reproduzindo..." ,i);
-                
-            }
-            
-        
         }
-        function playAway (){
+
+        function playAway {
             stopAll();
             away.play();
         }
-        function playTiringa (){
+
+        function playTiringa {
             stopAll();
             tiringa.play();
         }
+
+        function playsefodeo {
+            stopAll();
+            sefodeo.play();
+        }
+
+        function playcalaboca {
+            stopAll();
+            calaboca.play();
+        }
+
+        function playcreditos {
+            stopAll();
+            creditos.play();
+        }
+
 
         function stopAll (){
             dog.pause();
@@ -29,4 +44,11 @@ const dog = new Audio('./audio/oi.mp3');
             away.currentTime = 0;
             tiringa.pause();
             tiringa.currentTime = 0;
+            sefodeo.pause();
+            sefodeo.currentTime = 0;
+            calaboca.pause();
+            calaboca.currentTime = 0;
+            creditos.pause();
+            creditos.currentTime = 0;
+
         }
